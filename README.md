741e7f5c15ffbf0a58834d08986da474e64345fd
Rosana Valentina Peralta <rperalta@technisys.com>
2021-02-06 19:50:31 -0300

    add final commit, with read.me

---
+++ README.md
@@ -0,0 +1,23 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Getting Started
+
+First, install the package in /devartis:
+
+bash
+npm install
+# or
+yarn install
+
+Then, start the project with:
+
+bash
+npm start
+# or
+yarn start
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+
+
---
+++ devartis/src/components/navBar/index.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux"
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    backgroundColor: "#24262D",
+  },
+  link: {
+    textDecoration: "none",
+    color: "#FFFFFF"
+  }
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    // dispatch(fetchState())
+  }, [])
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <MenuIcon style={{ color: "#FFFFFF" }} />
+          </IconButton>
+          <Link to="/" className={classes.link}>
+            <Typography variant="h6" className={classes.title}>
+              RRSS
+          </Typography>
+          </Link>
+          <>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <Link to="/register" style={{
+                textDecoration: "none",
+                color: "inherit"
+              }}>
+                <MenuItem onClick={handleClose}>Register</MenuItem>
+              </Link>
+              <Link to="/logIn" style={{
+                textDecoration: "none",
+                color: "inherit"
+              }}>
+                <MenuItem onClick={handleClose}>Log-In</MenuItem>
+              </Link>
+
+            </Menu>
+          </>
+        </Toolbar>
+      </AppBar>
+    </div >
+  );
+}
+
